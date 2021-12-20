9b0cfabe7247d9f1382ce534e8cafffecf4f45f6
Peter Perhac <PeterPerhac@users.noreply.github.com>
2021-12-20 19:05:05 +0000

    i don't know... i guess i just broke it

--- src/main/scala/com/perhac/tools/smo/SmoReader.scala
+++ src/main/scala/com/perhac/tools/smo/SmoReader.scala
@@ -15,9 +15,9 @@ class SmoReader(intStream: LazyList[Int], file: File) {
       (Meta(expected, actual, SmsType(smsType), SmsStatus(smsStatus)), tail)
     }.toEither.leftMap(t => SmoParseError(file, t.getMessage))
 
-  def readFrom(stream: LazyList[Int]): Either[SmoReadError, (String, LazyList[Int])] =
+  def readFrom(stream: LazyList[Int], dropCount: Int): Either[SmoReadError, (String, LazyList[Int])] =
     Try {
-      val address = stream.drop(18)
+      val address = stream.drop(dropCount)
       val howMany = address.head
       val addressType = AddressType(address.drop(1).head)
       val bytesToRead = Math.ceil(howMany / 2).toInt
@@ -26,24 +26,38 @@ class SmoReader(intStream: LazyList[Int], file: File) {
       (formatPhoneNumber(addressType, telNum), address.drop(2 + bytesToRead))
     }.toEither.leftMap(t => SmoParseError(file, t.getMessage))
 
-  def readMessage(stream: LazyList[Int], meta: Meta): Either[SmoReadError, String] =
+  val accumulate: (StringBuilder, Int) => StringBuilder = {
+    case (sb, byte) =>
+      byte match {
+        case zero if zero == 0x00           => sb
+        case linebreak if linebreak == 0x0d => sb.append("\n")
+        case int                            => sb.append(int.toChar)
+      }
+  }
+
+  def readMessage(stream: LazyList[Int], meta: Meta): Either[SmoReadError, String] = {
+    def readNextMessage(segStream: LazyList[Int], mb: StringBuilder): LazyList[Int] = {
+      segStream.take(10).foreach(c => print( f"$c%02X "))
+      println()
+      val messageStart = readFrom(segStream, 11).right.get._2
+      messageStart.takeWhile(_ != 0xff).foldLeft(mb)(accumulate)
+      messageStart.takeWhile(_ != 0xff)
+    }
+
     Try {
       val msg1 = if (meta.partsExpected > 1) stream.drop(11) else stream.drop(4)
-      val messageBuilder = msg1.takeWhile(_ != 0xff).foldLeft(new StringBuilder()){
-        case (sb, byte) => byte match {
-          case zero if zero == 0x00 => sb
-          case linebreak if linebreak == 0x0d => sb.append("\n")
-          case int => sb.append(int.toChar)
-        }
-      }
+      val messageBuilder = msg1.takeWhile(_ != 0xff).foldLeft(new StringBuilder())(accumulate)
+
+      if (meta.partsExpected > 1) readNextMessage(msg1.dropWhile(_ != 0xff).dropWhile(_ == 0xff), messageBuilder)
 
       messageBuilder.toString()
     }.toEither.leftMap(t => SmoParseError(file, t.getMessage))
+  }
 
   def read(): Either[SmoReadError, SMS] =
     readMeta(intStream).flatMap {
       case (meta, stream1) =>
-        readFrom(stream1).flatMap {
+        readFrom(stream1, 18).flatMap {
           case (from, stream2) => readMessage(stream2, meta).map(message => SMS(file, from, meta, message))
         }
     }
